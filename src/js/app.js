562f78730895eaf752dd089ddb212c9fbc6cf1cd
whitemple <yarikkhromey@mail.ru>
2023-05-17 17:31:44 +0300

    create modal window

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,13 @@
 const API_KEY = "35382e03-7226-4c7b-9074-a5743781909c";
 const API_URL_POPULAR = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/top?type=TOP_250_BEST_FILMS&page=3';
 const API_URL_SEARCH = 'https://kinopoiskapiunofficial.tech/api/v2.1/films/search-by-keyword?keyword=';
+const API_URL_DETAILS = 'https://kinopoiskapiunofficial.tech/api/v2.2/films/';
 const movieEl = document.querySelector('.movies');
 const form = document.querySelector('.form');
 const searchInput = document.querySelector('.header__search');
+const modalWindow = document.querySelector('.modal');
+const uknownTime = 'Продолжительность неизвестна';
+const d = document.body;
 
 getMovie(API_URL_POPULAR);
 async function getMovie(url){
@@ -41,7 +45,7 @@ function createMovieCard(data){
             ${movie.rating && `<div class="movie__average movie__average--${madeRateColored(movie.rating)}">${movie.rating}</div>`}
         </div>
         `;
-
+        movieBox.addEventListener('click', () => showModal(movie.filmId));
         movieEl.append(movieBox);
     })
 }
@@ -56,5 +60,58 @@ form.addEventListener('submit', (e) => {
     }
     else{
         alert('Фильм не найден!');
+    }
+});
+
+// Modal Window
+function showModal(id){
+    async function openModal(id){
+        const response = await fetch(API_URL_DETAILS+id, {
+            headers:{
+                'Content-Type':'application/json',
+                'X-API-KEY': API_KEY
+            }
+        });
+        const responseData = await response.json();
+
+        modalWindow.classList.add('modal--show');
+        d.classList.add('stop-scrolling');
+        modalWindow.innerHTML=`
+            <div class="modal__card">
+                <img class="modal__movie-backdrop" src="${responseData.posterUrl}" alt="image">
+                <h2>
+                    <span class="modal__movie-title">${responseData.nameRu}</span>
+                    <span class="modal__movie-year">${responseData.year}</span>
+                </h2>
+                <ul class="modal__movie-info">
+                    <div class="loader"></div>
+                    <li class="modal__movie-genre">Жанр - ${responseData.genres.map((genre) => `<span>${genre.genre}</span>`)}</li>
+                    <li class="modal__movie-runtime">Время (мин) - ${responseData.filmLength || uknownTime}</li>
+                    <li>Сайт: <a href="${responseData.webUrl}" class="modal__movie-site">${responseData.webUrl}</a></li>
+                    <li class="modal__movie-overview">Описание - ${responseData.description}</li>
+                </ul>
+                <button type="button" class="modal__button-close">Закрыть</button>
+            </div>
+        `;
+        const btnClose = document.querySelector('.modal__button-close');
+        btnClose.addEventListener('click', () => closeModalWindow());
+    }
+    openModal(id);
+}
+
+function closeModalWindow(){
+    modalWindow.classList.remove('modal--show');
+    d.classList.remove('stop-scrolling');
+}
+
+window.addEventListener('click', (e) => {
+    if(e.target === modalWindow){
+        closeModalWindow();
+    }
+});
+
+window.addEventListener('keydown', (e) => {
+    if(e.code ==='Escape'){
+        closeModalWindow();
     }
 })
